733171af656fde56a73b2b92f7bc75df081e1100
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-01-02 21:20:45 +0600

    Update behavioral.v

--- combinational/full adder/behavioral.v
+++ combinational/full adder/behavioral.v
@@ -1,1 +1,9 @@
-
+module Full_Adder_Bh (s, cout, a, b, cin); 
+input a, b, cin; 
+output s, cout; 
+always @ (*) 
+begin 
+s = a ^ b ^ cin; 
+cout = (a & b) | (b & cin) | (cin & a); 
+end 
+endmodule 
